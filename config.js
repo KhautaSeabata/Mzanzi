4062465925646393b79375354587880b40f72b58
khauta seabata <75206578+KhautaSeabata@users.noreply.github.com>
2025-12-18 00:12:35 +0200

    Add Firebase configuration and initialization

---
+++ config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8Yh7DY90nujN6OiVD6kIHu3b-M0Kgero",
+  authDomain: "mzanzifx.firebaseapp.com",
+  databaseURL: "https://mzanzifx-default-rtdb.firebaseio.com",
+  projectId: "mzanzifx",
+  storageBucket: "mzanzifx.firebasestorage.app",
+  messagingSenderId: "589859282488",
+  appId: "1:589859282488:web:14f12501983b021cc1d0cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
